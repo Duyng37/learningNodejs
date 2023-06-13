b5db6ad2e32a23244696f02db73848e76561199f
duynk <duyyng37@gmail.com>
2023-06-13 15:00:51 +0700

    setup express handlers

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const handlebars = require("express-handlebars");
+
+//template engine
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources", "views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(3002);
